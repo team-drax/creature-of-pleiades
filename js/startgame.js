bb7c24788fdd031d39bd037fbfdf0fb6055ecbe0
Haustin Kimbrough <haustin.kimbrough@hotmail.com>
2021-08-02 23:14:50 -0700

    refactored startgame.js

--- js/startgame.js
+++ js/startgame.js
@@ -5,6 +5,8 @@
 //------------------------Global Variables----------------------------//
 let playScreen = document.querySelector('section1');
 let newGameButtonElem = document.getElementById('newGameButton');
+let startGameIdArr = ['name', 'gender', 'location', 'color'];
+let startGameTCArr = ['What\'s your name?', 'What\'s your gender?', 'Where are you located?', 'What\'s your favorite color?'];
 
 //------------------------Global Functions----------------------------//
 function handleStartGame(){
@@ -19,40 +21,21 @@ function handleStartGame(){
   let fieldsetElem = document.createElement('fieldset');
   let legendElem = document.createElement('legend');
   legendElem.textContent = 'Let\'s Get to Know You!';
-  fieldsetElem.appendChild(legendElem);
-  let labelElem = document.createElement('label');
-  labelElem.textContent = 'What\'s your name?';
-  fieldsetElem.appendChild(labelElem);
-  let inputElem = document.createElement('input');
-  inputElem.type = 'text';
-  inputElem.id = 'name';
-  labelElem.appendChild(inputElem); 
-  let label2Elem = document.createElement('label');
-  label2Elem.textContent = 'What\'s your gender?';
-  fieldsetElem.appendChild(label2Elem);
-  let input2Elem = document.createElement('input');
-  input2Elem.type = 'text';
-  input2Elem.id = 'gender';
-  label2Elem.appendChild(input2Elem);
-  let label3Elem = document.createElement('label');
-  label3Elem.textContent = 'Where are you located?';
-  fieldsetElem.appendChild(label3Elem);
-  let input3Elem = document.createElement('input');
-  input3Elem.type = 'text';
-  input3Elem.id = 'location';
-  label3Elem.appendChild(input3Elem);
-  let label4Elem = document.createElement('label');
-  label4Elem.textContent = 'What\'s your favorite color?';
-  fieldsetElem.appendChild(label4Elem);
-  let input4Elem = document.createElement('input');
-  input4Elem.type = 'text';
-  input4Elem.id = 'color';
-  label4Elem.appendChild(input4Elem);
+  fieldsetElem.appendChild(legendElem); 
+  for (let i = 0; i < startGameIdArr.length; i++){
+    let labelElem = document.createElement('label');
+    labelElem.textContent = startGameTCArr[i];
+    fieldsetElem.appendChild(labelElem);
+    let inputElem = document.createElement('input');
+    inputElem.type = 'text';
+    inputElem.id = startGameIdArr[i];
+    labelElem.appendChild(inputElem); 
+  }
   //Creates button to submit user responses and renders Chapter One text/story//
   let startChapterOneButton = document.createElement('button');
   startChapterOneButton.type = 'submit';
   startChapterOneButton.textContent = 'Continue';
-  startChapterOneButton.addEventListener('submit', handleStartChapterOne);
+  // startChapterOneButton.addEventListener('submit', handleStartChapterOne);
   startChapterOneButton.onclick(renderChapterOne());
 }
 
